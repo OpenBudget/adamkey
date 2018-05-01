62cd86a392915cd4c5cbf7ac421c171a6a98f431
Adam Kariv <adam.kariv@gmail.com>
2018-05-01 17:20:45 +0300

    Deploy to gh-pages

--- dist.sh
+++ dist.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-webpack
+git stash
+git checkout -b temp-deploy
 
-
+npm run build-app
+git add temp/
+git commit -m deploy-gh-pages
 git subtree push --prefix temp origin gh-pages
+rm -rf temp/
+
+git checkout master
+git branch -D temp-deploy
+git stash apply

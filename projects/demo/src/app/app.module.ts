d03f2e9e9c578b093493adf5c670b10fe9edf3ca
Adam Kariv <adam.kariv@gmail.com>
2025-01-05 09:22:48 +0200

    Bump to angular 18

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -6,23 +6,17 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 import { AdamkeyModule } from 'adamkey';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 declare const process: any;
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    AdamkeyModule,
-  ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ], imports: [BrowserModule,
+        FormsModule,
+        AdamkeyModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 }
